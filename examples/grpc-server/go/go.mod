a97a5f55ef8c5f9cc0e0fd285b5d30b577c4f08b
Lechuck Roh <lechuckroh@gmail.com>
2020-09-13 00:09:20 +0900

    create grpc-server in go

---
+++ examples/grpc-server/go/go.mod
@@ -0,0 +1,8 @@
+module github.com/lechuckroh/aws-lambda-grpc-proxy-go/examples/grpc-server/go
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.0
+	google.golang.org/grpc v1.32.0
+)
---
+++ examples/grpc-server/go/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/lechuckroh/aws-lambda-grpc-proxy-go/examples/grpc-server/go/hello"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	port = ":9090"
+)
+
+type HelloServer struct {
+}
+
+func (s *HelloServer) Call(ctx context.Context, req *hello.CallRequest) (*hello.CallResponse, error) {
+	return &hello.CallResponse{
+		Msg: fmt.Sprintf("hello %s", req.Name),
+	}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", port)
+	}
+	log.Printf("listening on %v", port)
+
+	server := grpc.NewServer()
+	hello.RegisterHelloServer(server, &HelloServer{})
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %+v", err)
+	}
+}
